91b43fd9d90caf231da3e0e01a0501342fbb02ee
luis ramirez <2023169@student.cct.ie>
2024-11-26 14:18:35 +0000

    adding first files

---
+++ index.js
@@ -0,0 +1,6 @@
+// Simulated CSV Data
+// Name, Age, Email, Phone Number, Eircode
+const csvData = `"John, Doe",30,"johndoe@example.com, 0893216548, 1YR5DD"
+"Jane, Smith","janesmith@example.com, 0892856548, 8MH7WE"
+"Michael, Johnson","michaeljohnson@example.com, 0898523694, 7RP0RR"
+"Tommy, Bean","michaeljohnson@example.com, 0894859612, EYR5DD"`
